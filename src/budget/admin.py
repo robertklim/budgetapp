24fb0f1dfa05cbea4bfe76d9d8e49722e9eb7016
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-24 09:46:18 +0200

    Create models (create, migrate, register in admin)

--- src/budget/admin.py
+++ src/budget/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Project, Expanse, Category
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Expanse)
+admin.site.register(Category)
--- src/budget/models.py
+++ src/budget/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128, unique=True, blank=True)
+    budget = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Project, self).save(*args, **kwargs)
+
+class Category(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+class Expanse(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
